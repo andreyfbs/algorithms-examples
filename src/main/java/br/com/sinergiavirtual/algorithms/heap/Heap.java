1d204faa8aa13ca4cc37025f08d98e734b1c967c
andreyfbs <andrey.barros.santos@gmail.com>
2017-02-04 18:49:35 -0200

    Heap - Heapify

--- src/main/java/br/com/sinergiavirtual/algorithms/heap/Heap.java
+++ src/main/java/br/com/sinergiavirtual/algorithms/heap/Heap.java
@@ -2,30 +2,83 @@ package br.com.sinergiavirtual.algorithms.heap;
 
 import java.util.Arrays;
 
+/**
+ * See definition of Heap : http://www.personal.kent.edu/~rmuhamma/Algorithms/MyAlgorithms/Sorting/heapSort.htm
+ */
 public class Heap {
 
-    private Integer[] heapData = new Integer[0];
+    private Integer[] heapData = new Integer[5];
     private Integer size = 0;
 
     public boolean add(Integer node) {
-        if (size == heapData.length) {
-            heapData = Arrays.copyOf(heapData, size + 1);
-        }
+        // check if parameter is null
         if (node == null) {
             return false;
         }
+
+        // check is full
+        if (isFull()) {
+            increaseCapacity();
+        }
+
+        // Set the new element to last of the Heap
         heapData[size] = node;
+
+        // Increment the size
         size++;
+
+        if (size > 1) {
+            // Heapify
+            Integer parentNode = (size / 2) -1;
+            Integer childNode = size -1;
+            while (parentNode != -1 && heapData[parentNode].compareTo(heapData[childNode]) < 0) {
+                Integer nodeTemp = heapData[parentNode];
+                heapData[parentNode] = heapData[childNode];
+                heapData[childNode] = nodeTemp;
+                childNode = parentNode;
+                parentNode = ((childNode + 1) / 2) - 1;
+            }
+        }
+
         return true;
     }
 
     public boolean contains(Integer node) {
 
         for (Integer data : heapData) {
-            if (data.equals(node)) {
+            if (node.equals(data)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public Integer containsWithPosition(Integer node) {
+        for (int position = 0; position < heapData.length; position++) {
+            if (node.equals(heapData[position])) {
+                return position;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isFull() {
+        if (size == heapData.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void increaseCapacity() {
+        heapData = Arrays.copyOf(heapData, size + 1);
+    }
+
+    @Override
+    public String toString() {
+        return "Heap{" +
+                "heapData=" + Arrays.toString(heapData) +
+                ", size=" + size +
+                '}';
     }
 }
--- src/test/java/br/com/sinergiavirtual/algorithms/heap/HeapTest.java
+++ src/test/java/br/com/sinergiavirtual/algorithms/heap/HeapTest.java
@@ -24,7 +24,7 @@ public class HeapTest {
         boolean result = heap.add(node);
 
         // Assert
-        Assert.assertTrue(result);
+        Assert.assertThat(result, Matchers.equalTo(Boolean.TRUE));
     }
 
     @Test
@@ -37,7 +37,7 @@ public class HeapTest {
         boolean result = heap.add(nodeNull);
 
         // Assert
-        Assert.assertFalse(result);
+        Assert.assertThat(result, Matchers.equalTo(Boolean.FALSE));
     }
 
     @Test
@@ -89,7 +89,7 @@ public class HeapTest {
         boolean result = heap.contains(node);
 
         // Assert
-        Assert.assertTrue(result);
+        Assert.assertThat(result, Matchers.equalTo(Boolean.TRUE));
 
     }
 
@@ -105,7 +105,47 @@ public class HeapTest {
         boolean result = heap.contains(nodeToSearch);
 
         // Assert
-        Assert.assertFalse(result);
+        Assert.assertThat(result, Matchers.equalTo(Boolean.FALSE));
+
+    }
+
+    @Test
+    public void getNotExistentElementEmptyList() {
+
+        // Arrange
+        Integer nodeToSearch = 1;
+
+        // Act
+        boolean result = heap.contains(nodeToSearch);
+
+        // Assert
+        Assert.assertThat(result, Matchers.equalTo(Boolean.FALSE));
+
+    }
+
+    @Test
+    public void checkPositionOfTheParent() {
+
+        // Arrange
+        int quantityOfElements = 9;
+        int parent = 5;
+        int child = 2;
+
+        for (int i = 1; i < quantityOfElements -1; i++) {
+            heap.add(i);
+        }
+
+        // Act
+        //Integer positionChild = heap.containsWithPosition(child);
+        Integer positionParent = heap.containsWithPosition(child);
+
+        // Assert
+        //Integer positionChildExpected = 5;
+        Integer positionParentExpected = 2;
+
+        System.out.println(heap.toString());
+
+        Assert.assertThat(positionParent, Matchers.equalTo(positionParentExpected));
 
     }
 
