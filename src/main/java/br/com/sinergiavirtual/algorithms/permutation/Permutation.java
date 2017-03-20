f8a94c780ebf3e33011e216bae4d6a51313e5676
andreyfbs <andrey.barros.santos@gmail.com>
2017-03-19 23:09:44 -0300

    The Shortest String.

---
+++ src/main/java/br/com/sinergiavirtual/algorithms/permutation/Permutation.java
@@ -0,0 +1,38 @@
+package br.com.sinergiavirtual.algorithms.permutation;
+
+/**
+ * Created by andrey on 19/03/17.
+ */
+public class Permutation {
+
+    public static void main(String[] args) {
+        String str = "abcd";
+        permutation(str);
+    }
+
+    public static void permutation(String str) {
+        permutation("", str);
+    }
+
+    private static void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) {
+            System.out.println("-------RESP----------: " + prefix);
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                System.out.println("INITIAL: " + i + " N: " + n);
+                String head = prefix + str.charAt(i);
+                System.out.println("Prefix: " + prefix + " Str: " + str);
+                String tail = str.substring(0, i) + str.substring(i + 1, n);
+                System.out.println("Head: " + head +  " Tail: " + tail);
+                System.out.println("FINAL: " + i + " N: " + n);
+                System.out.println("-----------------------");
+
+                permutation(head, tail);
+            }
+
+        }
+    }
+
+}
---
+++ src/test/java/br/com/sinergiavirtual/algorithms/permutation/ShortestStringTest.java
@@ -0,0 +1,28 @@
+package br.com.sinergiavirtual.algorithms.permutation;
+
+import br.com.sinergiavirtual.algorithms.string.ShortestString;
+import br.com.sinergiavirtual.algorithms.string.ShortestString2;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShortestStringTest {
+
+    @Test
+    public void shortestSubstrContainingAllChars() throws Exception {
+        ShortestString shortestString = new ShortestString2();
+
+        String result = shortestString.shortestSubstrContainingAllChars("acbbaca", "aba");
+        assertThat(result, Matchers.equalTo("baca"));
+
+        result = shortestString.shortestSubstrContainingAllChars("acbbADOBECODEBANCaca", "ABC");
+        assertThat(result, Matchers.equalTo("BANC"));
+
+        result = shortestString.shortestSubstrContainingAllChars("this is a test string", "tist");
+        assertThat(result, Matchers.equalTo("t stri"));
+
+        result = shortestString.shortestSubstrContainingAllChars("cacd", "cd");
+        assertThat(result, Matchers.equalTo("cd"));
+    }
+}
