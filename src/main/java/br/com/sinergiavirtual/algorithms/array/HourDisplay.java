3e48a072760d197d492877b4901de84b11de64c8
andreyfbs <andrey.barros.santos@gmail.com>
2017-12-09 18:21:07 -0200

    Refactoring

--- src/main/java/br/com/sinergiavirtual/algorithms/array/HourDisplay.java
+++ src/main/java/br/com/sinergiavirtual/algorithms/array/HourDisplay.java
@@ -5,93 +5,107 @@ import java.util.*;
 public class HourDisplay {
     public String calculateShortestTimeDisplay(int A, int B, int C, int D, int E, int F) {
 
+        // Put elements in array
         Integer[] inputArray = {A, B, C, D, E, F};
 
-        Integer[] inputArraySorted = inputArray;
-        Arrays.sort(inputArraySorted);
+        // Sort this array
+        Arrays.sort(inputArray);
 
-        List<Integer> orderList = new ArrayList<>();
-        for (int i = 0; i < inputArraySorted.length; i++) {
-            orderList.add(inputArraySorted[i]);
-        }
-        Integer[] earliestTimeArray = new Integer[orderList.size()];
+        // Final array
+        Integer[] earliestTimeArray = new Integer[inputArray.length];
 
-        Set<Integer> h1 = new HashSet<>();
-        h1.add(0);
-        h1.add(1);
+        // Prepare the sets
 
-        Set<Integer> h1Special = new HashSet<>();
-        h1Special.add(2);
+        // Represents the letter N
 
-        Set<Integer> h2Special = new HashSet<>();
-        h2Special.add(0);
-        h2Special.add(1);
-        h2Special.add(2);
-        h2Special.add(3);
+        // NX:XX:XX
+        Set<Integer> firstNumberFirstPossibilitiesSet = new HashSet<>();
+        firstNumberFirstPossibilitiesSet.add(0);
+        firstNumberFirstPossibilitiesSet.add(1);
 
-        Set<Integer> defaultPart1 = new HashSet<>();
-        defaultPart1.add(0);
-        defaultPart1.add(1);
-        defaultPart1.add(2);
-        defaultPart1.add(3);
-        defaultPart1.add(4);
-        defaultPart1.add(5);
+        // NX:XX:XX
+        Set<Integer> firstNumberSecondPossibilitiesSet = new HashSet<>();
+        firstNumberSecondPossibilitiesSet.add(2);
 
-        Set<Integer> defaultPart2 = new HashSet<>();
-        defaultPart2.add(0);
-        defaultPart2.add(1);
-        defaultPart2.add(2);
-        defaultPart2.add(3);
-        defaultPart2.add(4);
-        defaultPart2.add(5);
-        defaultPart2.add(6);
-        defaultPart2.add(7);
-        defaultPart2.add(8);
-        defaultPart2.add(9);
+        // XN:XX:XX
+        Set<Integer> secondNumberSecondPossibilitiesSet = new HashSet<>();
+        secondNumberSecondPossibilitiesSet.add(0);
+        secondNumberSecondPossibilitiesSet.add(1);
+        secondNumberSecondPossibilitiesSet.add(2);
+        secondNumberSecondPossibilitiesSet.add(3);
+
+        // XX:NX:NX
+        Set<Integer> defaultFirstNumberSet = new HashSet<>();
+        defaultFirstNumberSet.add(0);
+        defaultFirstNumberSet.add(1);
+        defaultFirstNumberSet.add(2);
+        defaultFirstNumberSet.add(3);
+        defaultFirstNumberSet.add(4);
+        defaultFirstNumberSet.add(5);
+
+        // XN:XN:XN
+        Set<Integer> defaultSecondNumberSet = new HashSet<>();
+        defaultSecondNumberSet.add(0);
+        defaultSecondNumberSet.add(1);
+        defaultSecondNumberSet.add(2);
+        defaultSecondNumberSet.add(3);
+        defaultSecondNumberSet.add(4);
+        defaultSecondNumberSet.add(5);
+        defaultSecondNumberSet.add(6);
+        defaultSecondNumberSet.add(7);
+        defaultSecondNumberSet.add(8);
+        defaultSecondNumberSet.add(9);
 
         Map<Integer, Set<Integer>> hourPositionsSet1 = new HashMap<>();
-        hourPositionsSet1.put(0, h1);
-        hourPositionsSet1.put(1, defaultPart2);
-        hourPositionsSet1.put(2, defaultPart1);
-        hourPositionsSet1.put(3, defaultPart2);
-        hourPositionsSet1.put(4, defaultPart1);
-        hourPositionsSet1.put(5, defaultPart2);
+        hourPositionsSet1.put(0, firstNumberFirstPossibilitiesSet);
+        hourPositionsSet1.put(1, defaultSecondNumberSet);
+        hourPositionsSet1.put(2, defaultFirstNumberSet);
+        hourPositionsSet1.put(3, defaultSecondNumberSet);
+        hourPositionsSet1.put(4, defaultFirstNumberSet);
+        hourPositionsSet1.put(5, defaultSecondNumberSet);
 
         Map<Integer, Set<Integer>> hourPositionsSet2 = new HashMap<>();
-        hourPositionsSet2.put(0, h1Special);
-        hourPositionsSet2.put(1, h2Special);
-        hourPositionsSet2.put(2, defaultPart1);
-        hourPositionsSet2.put(3, defaultPart2);
-        hourPositionsSet2.put(4, defaultPart1);
-        hourPositionsSet2.put(5, defaultPart2);
+        hourPositionsSet2.put(0, firstNumberSecondPossibilitiesSet);
+        hourPositionsSet2.put(1, secondNumberSecondPossibilitiesSet);
+        hourPositionsSet2.put(2, defaultFirstNumberSet);
+        hourPositionsSet2.put(3, defaultSecondNumberSet);
+        hourPositionsSet2.put(4, defaultFirstNumberSet);
+        hourPositionsSet2.put(5, defaultSecondNumberSet);
 
         Map<Integer, Set<Integer>> hourPositions = new HashMap<>();
 
-        for (int i = 0; i < orderList.size(); i++) {
+        for (int i = 0; i < inputArray.length; i++) {
+            final int currentValue = inputArray[i];
+
+            // Check the First Part of the Hour to define the specific Set
             if (i == 0) {
-                if (hourPositionsSet1.get(i).contains(orderList.get(i))) {
+                if (hourPositionsSet1.get(i).contains(currentValue)) {
                     hourPositions = hourPositionsSet1;
-                } else if (hourPositionsSet2.get(i).contains(orderList.get(i))) {
+                } else if (hourPositionsSet2.get(i).contains(currentValue)) {
                     hourPositions = hourPositionsSet2;
                 } else {
                     return "IMPOSSIBLE TO FORMAT";
                 }
-                // hourSpecialPositions
             }
-            if (hourPositions.get(i).contains(orderList.get(i))) {
-                earliestTimeArray[i] = orderList.get(i);
+
+            // If is a valid value to position in the hour
+            if (hourPositions.get(i).contains(currentValue)) {
+                earliestTimeArray[i] = currentValue;
             } else {
+                // Try to find the closest Neighbor that has the number that can change
                 boolean changePosition = false;
                 for (int j = i - 1; j > 0; j--) {
 
+                    // If found a Neighbor
                     if (hourPositions.get(i).contains(earliestTimeArray[j])
-                            && hourPositions.get(j).contains(orderList.get(i))) {
+                            && hourPositions.get(j).contains(currentValue)) {
                         earliestTimeArray[i] = earliestTimeArray[j];
-                        earliestTimeArray[j] = orderList.get(i);
+                        earliestTimeArray[j] = currentValue;
                         changePosition = true;
                         break;
                     }
                 }
+                // Else, it's not possible to change with anyone, so impossible to format a valid hour
                 if (!changePosition) {
                     return "IMPOSSIBLE TO FORMAT";
                 }
